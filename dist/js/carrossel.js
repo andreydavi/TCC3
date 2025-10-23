64a558c90c256dea669a2d9737c504fcb67348c1
andreydavi <andreydavi3ao@gmail.com>
2025-10-22 22:59:11 -0300

    01 - Reestruturando e melhorando todo o site

---
+++ dist/js/carrossel.js
@@ -0,0 +1,13 @@
+const carousel = document.getElementById('carousel');
+  const slides = carousel.children.length;
+  let index = 0;
+
+  document.getElementById('next').addEventListener('click', () => {
+    index = (index + 1) % slides;
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+  });
+
+  document.getElementById('prev').addEventListener('click', () => {
+    index = (index - 1 + slides) % slides;
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+  });
--- dist/js/sanduiche.js
+++ dist/js/sanduiche.js
@@ -1,34 +1,18 @@
-const menuToggle = document.getElementById("menu-toggle");
-  const menuIcon = document.getElementById("menu-icon");
-  const menu = document.getElementById("menu");
-  const btnTheme = document.getElementById("btnTheme");
-  const btnThemeMobile = document.getElementById("btnThemeMobile");
-  const html = document.documentElement;
+const navbar = document.querySelector('#navbar');
+const btnOpenNavbar = document.querySelector('#open-navbar');
+const closeNavbar = document.querySelector('#close-navbar');
 
-  // Alternar menu
-  menuToggle.addEventListener("click", () => {
-    const isHidden = menu.classList.contains("hidden");
+btnOpenNavbar.addEventListener('click', () => {
+  navbar.classList.remove('hidden');
+},);
 
-    if (isHidden) {
-      // Mostrar menu
-      menu.classList.remove("hidden");
-      menu.classList.add("flex", "animate-slideDown");
-      menuIcon.innerHTML = `
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          d="M6 18L18 6M6 6l12 12" />
-      `;
-    } else {
-      // Fechar menu
-      menu.classList.add("animate-slideUp");
-      setTimeout(() => {
-        menu.classList.remove("flex", "animate-slideDown", "animate-slideUp");
-        menu.classList.add("hidden");
-      }, 300);
-      menuIcon.innerHTML = `
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          d="M4 6h16M4 12h16M4 18h16" />
-      `;
-    }
+closeNavbar.addEventListener('click', () => {
+  navbar.classList.add('hidden');
+},);
+
+const menuLinks = navbar.querySelectorAll('a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    navbar.classList.add('hidden');
   });
-
-  
+});
