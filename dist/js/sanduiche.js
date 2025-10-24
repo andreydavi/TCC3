be5ac5cab9d9b9ccad69a2afcad3b070e4b3bd0a
andreydavi <andreydavi3ao@gmail.com>
2025-10-24 09:10:16 -0300

    03 - Criando seção de imagens e diferencial

--- dist/js/sanduiche.js
+++ dist/js/sanduiche.js
@@ -16,3 +16,16 @@ menuLinks.forEach(link => {
     navbar.classList.add('hidden');
   });
 });
+
+ const cards = document.querySelectorAll('.card');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove('opacity-0', 'translate-y-8');
+        entry.target.classList.add('opacity-100', 'translate-y-0');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 });
+
+  cards.forEach(card => observer.observe(card));
